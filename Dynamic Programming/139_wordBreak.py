ad8986673df995595050ba280c4fc631df0468af
puhung <twenet1212@gmail.com>
2023-05-17 13:43:55 -0700

    update wordBreak

--- Dynamic Programming/139_wordBreak.py
+++ Dynamic Programming/139_wordBreak.py
@@ -9,3 +9,15 @@ class Solution:
                 if s[start: i+1] == w and (dp[i-len(w)] == True or i-len(w) == -1):
                     dp[i] = True
         return dp[-1]
+    
+# The idea is the following:
+# d is an array that contains booleans
+# d[i] is True if there is a word in the dictionary that ends at ith index of s AND d is also True at the beginning of the word
+
+# Example:
+# s = "leetcode"
+# words = ["leet", "code"]
+
+# d[3] is True because there is "leet" in the dictionary that ends at 3rd index of "leetcode"
+# i == 3 and 3 - len(leet) == -1
+# d[7] is True because there is "code" in the dictionary that ends at the 7th index of "leetcode" AND d[3] is True
